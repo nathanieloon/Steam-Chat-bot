76a9613f7e6d3ab6e74e82b507b42d075a66b744
snurton <aspen_mountains@hotmail.com>
2013-11-13 15:14:32 +1100

    Added jquery mutation observer for new branch

---
+++ jquery.mutationobserver.js
@@ -0,0 +1,40 @@
+/**
+ *	jQuery mutationObserver 1.0.2
+ *	https://github.com/timbonicus/jquery-mutationobserver
+ *
+ *	Dual licensed under the MIT and GPL licenses.
+ *	http://en.wikipedia.org/wiki/MIT_License
+ *	http://en.wikipedia.org/wiki/GNU_General_Public_License
+ */
+(function($) {
+    var jQueryMutationFns = ['after', 'append', 'before', 'empty', 'html', 'prepend', 'remove']
+    var mutationObservers = []
+    var __mutationTimeout
+
+    $.each(jQueryMutationFns, function(i, fn) {
+        var originalFn = $.fn[fn]
+        $.fn[fn] = function() {
+            var me = this
+            var result = originalFn.apply(this, arguments)
+            clearTimeout(__mutationTimeout)
+            __mutationTimeout = setTimeout(function() { fire(me) }, 50)
+            return result
+        }
+    })
+
+    function fire($element) {
+        $.each(mutationObservers, function(i, observer) {
+            if ($element.closest(observer.el).length)
+                observer.listener()
+        })
+    }
+
+    $.fn.mutationObserver = function(listenerFn) {
+        if (this.length == 0)
+            return this
+        if (this.length > 1)
+            return $.each(this, function(i, obj) { $(obj).mutationObserver(listenerFn) })
+
+        mutationObservers.push({el: this, listener: $.proxy(listenerFn, this)})
+    }
+}(jQuery));
